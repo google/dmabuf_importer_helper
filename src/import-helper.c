37b118cfa11575834a522a85f734782a65c1e260
Samiullah Khawaja <skhawaja@google.com>
2024-01-11 19:45:21 +0000

    Use GPL2 text in headers
    Change-Id: Iff50610f7f3bed4c7afc2d8191e993d5b0b8d13f

--- src/import-helper.c
+++ src/import-helper.c
@@ -1,21 +1,14 @@
 /*
  * Copyright 2024 Google LLC
- * Author: Samiullah Khawaja
  *
- * This file is part of Dmabuf Import Helper
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
  *
- * Dmabuf Import Helper is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * Dmabuf Import Helper is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Dmabuf Import Helper. If not, see <https://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
  */
 
